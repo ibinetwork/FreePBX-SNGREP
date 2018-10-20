986ee75afb6f375ba9cf59401c13b1224917b613
Rafael Tavares <rafael@ibinetwork.com.br>
2018-10-20 17:52:19 -0300

    Create sng_install.sh

---
+++ sng_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Instalando sngrep"
+echo "" 
+rm -Rf /etc/yum.repos.d/irontec.repo
+cat > /etc/yum.repos.d/irontec.repo <<EOF
+[irontec]
+name=Irontec RPMs repository
+baseurl=http://packages.irontec.com/centos/\$releasever/\$basearch/
+EOF
+rpm --import http://packages.irontec.com/public.key
+yum install sngrep -y
