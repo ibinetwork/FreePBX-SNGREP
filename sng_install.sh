0f50c21e0dec1bc8561e3b4f5e932d0c25b7d6b2
Rafael Tavares <rafael@ibinetwork.com.br>
2020-03-12 21:14:29 -0300

    Update Repositório
    add gpgcheck=0

--- sng_install.sh
+++ sng_install.sh
@@ -6,6 +6,7 @@ cat > /etc/yum.repos.d/irontec.repo <<EOF
 [irontec]
 name=Irontec RPMs repository
 baseurl=http://packages.irontec.com/centos/\$releasever/\$basearch/
+gpgcheck=0
 EOF
 rpm --import http://packages.irontec.com/public.key
 yum install sngrep -y
