5956d70b32e8a04fd6da6bbc84035d635f27b22a
Rafael Tavares <rafael@ibinetwork.com.br>
2022-06-08 18:16:19 -0300

    Update sngrep repo

--- sng_install.sh
+++ sng_install.sh
@@ -3,10 +3,16 @@ echo "Instalando sngrep"
 echo "" 
 rm -Rf /etc/yum.repos.d/irontec.repo
 cat > /etc/yum.repos.d/irontec.repo <<EOF
-[irontec]
-name=Irontec RPMs repository
-baseurl=http://packages.irontec.com/centos/\$releasever/\$basearch/
-gpgcheck=0
+[copr:copr.fedorainfracloud.org:irontec:sngrep]
+name=Copr repo for sngrep owned by irontec
+baseurl=https://download.copr.fedorainfracloud.org/results/irontec/sngrep/epel-7-$basearch/
+type=rpm-md
+skip_if_unavailable=True
+gpgcheck=1
+gpgkey=https://download.copr.fedorainfracloud.org/results/irontec/sngrep/pubkey.gpg
+repo_gpgcheck=0
+enabled=1
+enabled_metadata=1
 EOF
 rpm --import http://packages.irontec.com/public.key
 yum install sngrep -y
